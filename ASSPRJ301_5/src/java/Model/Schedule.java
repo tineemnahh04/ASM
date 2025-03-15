8254c519ccf93a3e7ef8d05160f70256461914e8
tineemnahh04 <manhdthe186944@fpt.edu.vn>
2025-03-16 04:13:32 +0700

    Create Schedule

---
+++ ASSPRJ301_5/src/java/Model/Schedule.java
@@ -0,0 +1,66 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Model;
+
+import java.util.Date;
+
+/**
+ *
+ * @author admin
+ */
+public class Schedule {
+    private int id;
+    private int employeeId;
+    private Date date;
+    private boolean status;
+
+    public Schedule() {
+    }
+
+    public Schedule(int id, int employeeId, Date date, boolean status) {
+        this.id = id;
+        this.employeeId = employeeId;
+        this.date = date;
+        this.status = status;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getEmployeeId() {
+        return employeeId;
+    }
+
+    public void setEmployeeId(int employeeId) {
+        this.employeeId = employeeId;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public boolean isStatus() {
+        return status;
+    }
+
+    public void setStatus(boolean status) {
+        this.status = status;
+    }
+
+    @Override
+    public String toString() {
+        return "Schedule{" + "id=" + id + ", employeeId=" + employeeId + ", date=" + date + ", status=" + status + '}';
+    }
+    
+}
