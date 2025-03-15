8132e73f9754095b718ea47976299b87016a9778
tineemnahh04 <manhdthe186944@fpt.edu.vn>
2025-03-16 04:16:24 +0700

    Create RoleDAO

---
+++ ASSPRJ301_5/src/java/Dal/RoleDAO.java
@@ -0,0 +1,44 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Dal;
+
+import Model.Role;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ *
+ * @author admin
+ */
+public class RoleDAO {
+    DBContext db=new DBContext();
+    
+    public List<Role> getAllRole() {
+        List<Role> list = new ArrayList<>();
+        String sql = "select * from [Role]";
+        try {
+            
+            PreparedStatement st = db.connection.prepareStatement(sql);
+            ResultSet rs = st.executeQuery();
+            while (rs.next()) {
+                Role rl=new Role();
+                rl.setId(rs.getInt(1));
+                rl.setName(rs.getString(2));
+                rl.setDescription(rs.getString(3));                
+                list.add(rl);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return list;
+    }
+    public static void main(String[] args) {
+        RoleDAO rl=new RoleDAO();
+        List<Role> l=rl.getAllRole();
+        System.out.println(l.get(0));
+}
+}
