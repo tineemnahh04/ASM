2921d6c17c0ccd594b7b5e6c5fdbc7001aec1c9d
tineemnahh04 <manhdthe186944@fpt.edu.vn>
2025-03-16 04:16:38 +0700

    Create ScheduleDAO

---
+++ ASSPRJ301_5/src/java/Dal/ScheduleDAO.java
@@ -0,0 +1,70 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Dal;
+
+import Model.ScheduleDTO;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ *
+ * @author admin
+ */
+public class ScheduleDAO {
+    DBContext db=new DBContext();
+     public List<ScheduleDTO> getSchedule() {
+        List<ScheduleDTO> list = new ArrayList<>();
+        String sql = "select  sche.Id,sche.Date,sche.Status,e.Name from Schedule sche inner join Employee e on e.Id = sche.EmployeeId";
+        try {
+
+            PreparedStatement st = db.connection.prepareStatement(sql);
+            ResultSet rs = st.executeQuery();
+            while (rs.next()) {
+                ScheduleDTO s = new ScheduleDTO();
+                s.setId(rs.getInt(1));
+                s.setDate(rs.getDate(2));
+                s.setStatus(rs.getBoolean(3));
+                s.seteName(rs.getString(4));
+                list.add(s);
+            }
+        } catch (SQLException ex) {
+        }
+        return list;
+    }
+     public List<ScheduleDTO> getSchedulebyManagerID( int EmployeeId ) {
+        List<ScheduleDTO> list = new ArrayList<>();
+        String sql = "select  sche.Id,sche.Date,sche.Status,e.Name from Schedule sche inner join Employee e on e.Id"
+                + " = sche.EmployeeId where e.Parentemployee = ?";
+        try {
+
+            PreparedStatement st = db.connection.prepareStatement(sql);
+            st.setInt(1,EmployeeId );
+            ResultSet rs = st.executeQuery();
+            
+            while (rs.next()) {
+                ScheduleDTO s = new ScheduleDTO();
+                s.setId(rs.getInt(1));
+                s.setDate(rs.getDate(2));
+                s.setStatus(rs.getBoolean(3));
+                s.seteName(rs.getString(4));
+                list.add(s);
+            }
+        } catch (SQLException ex) {
+        }
+        return list;
+    }
+     public static void main(String[] args) {
+        ScheduleDAO r=new ScheduleDAO();
+        List<ScheduleDTO> l=r.getSchedulebyManagerID(2);
+        
+        for (ScheduleDTO scheduleDTO : l) {
+            System.out.println(scheduleDTO.getId()+" "+scheduleDTO.getDate());
+        }
+}
+
+}
