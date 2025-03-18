339e37af0fc4a591b1f1bd3fd96557fb7a697f75
tineemnahh04 <manhdthe186944@fpt.edu.vn>
2025-03-19 06:20:03 +0700

    Update RoleDAO.java

--- ASSPRJ301_5/src/java/Dal/RoleDAO.java
+++ ASSPRJ301_5/src/java/Dal/RoleDAO.java
@@ -9,6 +9,7 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.util.ArrayList;
 import java.util.List;
+import java.sql.*;
 
 /**
  *
@@ -18,24 +19,22 @@ public class RoleDAO {
     DBContext db=new DBContext();
     
     public List<Role> getAllRole() {
-        List<Role> list = new ArrayList<>();
-        String sql = "select * from [Role]";
-        try {
-            
-            PreparedStatement st = db.connection.prepareStatement(sql);
-            ResultSet rs = st.executeQuery();
-            while (rs.next()) {
-                Role rl=new Role();
-                rl.setId(rs.getInt(1));
-                rl.setName(rs.getString(2));
-                rl.setDescription(rs.getString(3));                
-                list.add(rl);
-            }
-        } catch (Exception e) {
-            e.printStackTrace();
+    List<Role> list = new ArrayList<>();
+    String sql = "select * from [Role]";
+    try (PreparedStatement st = db.connection.prepareStatement(sql);
+         ResultSet rs = st.executeQuery()) {
+        while (rs.next()) {
+            Role rl = new Role();
+            rl.setId(rs.getInt(1));
+            rl.setName(rs.getString(2));
+            rl.setDescription(rs.getString(3));
+            list.add(rl);
         }
-        return list;
+    } catch (SQLException e) {
+        e.printStackTrace();
     }
+    return list;
+}
     public static void main(String[] args) {
         RoleDAO rl=new RoleDAO();
         List<Role> l=rl.getAllRole();
