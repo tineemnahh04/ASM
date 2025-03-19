d29171e3af0e80a81e89ca8c76d23dbe31d4740a
tineemnahh04 <manhdthe186944@fpt.edu.vn>
2025-03-20 04:52:10 +0700

    Update AccountDAO.java

--- ASSPRJ301_5/src/java/Dal/AccountDAO.java
+++ ASSPRJ301_5/src/java/Dal/AccountDAO.java
@@ -3,17 +3,21 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package Dal;
+
 import Model.Account;
 import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
+
 /**
  *
  * @author admin
  */
-public class AccountDAO extends DBContext{
+public class AccountDAO extends DBContext {
+
     DBContext db = new DBContext();
-    public Account validateUser(String username, String password ){
+
+    public Account validateUser(String username, String password) {
         String sql = "select * from Account "
                 + "where Username = ? AND Password = ? ";
         try {
@@ -21,27 +25,28 @@ public class AccountDAO extends DBContext{
             ps.setString(1, username);
             ps.setString(2, password);
             ResultSet rs = ps.executeQuery();
-            while(rs.next()){
-                Account acc = new Account();
-                acc.setId(rs.getInt(1));
-                acc.setUsername(rs.getString(2));
-                acc.setPassword(rs.getString(3));
-                acc.setEmployeeId(rs.getInt(4));
-                acc.setRoleId(rs.getInt(5));
-                return acc;
+            while (rs.next()) {
+                Account account = new Account();
+                account.setId(rs.getInt(1));
+                account.setUsername(rs.getString(2));
+                account.setPassword(rs.getString(3));
+                account.setEmployeeId(rs.getInt(4));
+                account.setRoleId(rs.getInt(5));
+                return account;
             }
         } catch (SQLException ex) {
-        
+           
+        }
+        return null;
     }
-    return null;
-}
-    public List<Account> getAllAccount(){
+    public List<Account> getAllAccount() {
         List<Account> list = new ArrayList<>();
-        String sql = "Select * from [Account]";
+        String sql = "select * from [Account]";
         try {
+
             PreparedStatement st = db.connection.prepareStatement(sql);
             ResultSet rs = st.executeQuery();
-            while(rs.next()){
+            while (rs.next()) {
                 Account a = new Account();
                 a.setId(rs.getInt(1));
                 a.setUsername(rs.getString(2));
@@ -50,32 +55,38 @@ public class AccountDAO extends DBContext{
                 a.setRoleId(rs.getInt(5));
                 list.add(a);
             }
-        } catch (Exception e) {
+        } catch (SQLException ex) {
         }
         return list;
     }
-    
-    public Account getAccount(String Username, String Password){
+
+    public Account getAccount(String Username, String Password) {
         String sql = "Select * from Account where Username=? and Password=?";
         try {
-            PreparedStatement ps = db.connection.prepareStatement(sql);
-            ps.setString(1, Username);
-            ps.setString(2, Password);
-            ResultSet rs = ps.executeQuery();
-            while(rs.next()){
+            PreparedStatement pr = db.connection.prepareStatement(sql);
+            pr.setString(1, Username);
+            pr.setString(2, Password);
+            ResultSet rs = pr.executeQuery();
+            while (rs.next()) {
                 Account acc = new Account();
                 acc.setId(rs.getInt(1));
                 acc.setUsername(rs.getString(2));
                 acc.setPassword(rs.getString(3));
                 acc.setEmployeeId(rs.getInt(4));
-                acc.setRoleId(rs.getInt(5));
                 return acc;
             }
         } catch (SQLException ex) {
+
         }
         return null;
     }
+
+
     public static void main(String[] args) {
-        
+        AccountDAO accd = new AccountDAO();
+        List<Account> l = accd.getAllAccount();
+        for (Account a : l) {
+            System.out.println(l);
+        }
     }
 }
